e2faf0a83f5347b4e4393db80616799e19c47acd
Luis1080931 <luisu0616@gmail.com>
2023-12-05 11:08:18 -0500

    login

--- sistema-gestion-universitaria/resources/views/dashboard.blade.php
+++ sistema-gestion-universitaria/resources/views/dashboard.blade.php
@@ -1,17 +1,45 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('listado de estudiantes') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <body class="bg-gray-100 p-4">
+
+        <div class="max-w-2xl mx-auto">
+    
+            <!-- Formulario para registrar usuarios -->
+            <form id="userForm" class="mb-4">
+                <div class="mb-4">
+                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nombre:</label>
+                    <input type="text" id="name" name="name" class="w-full border p-2">
                 </div>
-            </div>
+                <div class="mb-4">
+                    <label for="course" class="block text-gray-700 text-sm font-bold mb-2">Curso:</label>
+                    <input type="text" id="course" name="course" class="w-full border p-2">
+                </div>
+                <div class="mb-4">
+                    <label for="professor" class="block text-gray-700 text-sm font-bold mb-2">Profesor:</label>
+                    <input type="text" id="professor" name="professor" class="w-full border p-2">
+                </div>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Registrar Usuario</button>
+            </form>
+    
+            <!-- Tabla para listar usuarios -->
+            <table id="userTable" class="min-w-full bg-white border border-gray-300">
+                <thead>
+                    <tr>
+                        <th class="border-b p-2">ID</th>
+                        <th class="border-b p-2">Nombre</th>
+                        <th class="border-b p-2">Curso</th>
+                        <th class="border-b p-2">Profesor</th>
+                        <th class="border-b p-2">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Aquí se listarán dinámicamente los usuarios -->
+                </tbody>
+            </table>
         </div>
-    </div>
 </x-app-layout>
--- sistema-gestion-universitaria/routes/web.php
+++ sistema-gestion-universitaria/routes/web.php
@@ -1,8 +1,44 @@
 <?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\PermissionsController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\DesignationController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SalarySlipController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\PayrollController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\AccountingController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\VoucherController;
+use App\Http\Controllers\LedgerController;
+use App\Http\Controllers\JournalController;
+use App\Http\Controllers\TrialBalanceController;
+use App\Http\Controllers\GeneralLedgerController;
+use App\Http\Controllers\CashBookController;
+use App\Http\Controllers\BankStatementController;
+use App\Http\Controllers\PettyCashController;
+use App\Http\Controllers\PurchaseOrderController;
+use App\Http\Controllers\SupplierPaymentController;
+use App\Http\Controllers\CustomerReceiptController;
+use App\Http\Controllers\CustomerDueController;
+use App\Http\Controllers\StockReportController;
+use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockAdjustmentController;
+use App\Http\Controllers\StockTransferController;
+
 use Illuminate\Support\Facades\Route;
 use Laravel\Breeze\Http\Controllers\AuthenticatedSessionController;
 use Laravel\Breeze\Http\Controllers\RegisteredUserController;
@@ -25,25 +61,51 @@ Route::middleware(['guest'])->group(function () {
 
 Route::middleware(['auth'])->group(function () {
     // Your other routes here
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
-    Route::resource('/users', App\Http\Controllers\UsersController::class);
-    Route::resource('/roles', App\Http\Controllers\RolesController::class);
-    Route::resource('/permissions', App\Http\Controllers\PermissionsController::class);
+    Route::view('/dashboard', 'admin.index')->name('dashboard');
 
-    Route::prefix('admin')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'index'])->name('profile.show');
-        Route::put('/profile/edit/{id}', [ProfileController::class, 'update'])->name('profile.update');
-        Route::put('/profile/change_pass/{id}', [ProfileController::class, 'changePass'])->name('profile.change_pass');
-        Route::delete('/profile/destroy/{id}', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        Route::get('/user/list', [AdminController::class, 'index'])->name('user.index');
-        Route::get('/role/list', [RoleController::class, 'index'])->name('role.index');
-        // ...
+    Route::prefix('profile')->group(function () {
+        Route::get('/edit/{id}', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::put('/update/{id}', [ProfileController::class, 'update'])->name('profile.update');
+    });
+
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UsersController::class);
+
+    Route::get('/logout', function () {
+        Auth::guard('web')->logout();
+        return redirect()->route('login');
+    })->name('logout');
+
+    Route::prefix('users')->group(function () {
+        Route::get('/list', [AdminController::class, 'userList'])->name('users.list');
+        Route::get('/add', [AdminController::class, 'addUsers'])->name('users.add');
+        Route::post('/save', [AdminController::class, 'saveUsers'])->name('users.save');
+        Route::get('/delete/{id}', [AdminController::class, 'deleteUsers'])->name('users.delete');
+        Route::get('/edit/{id}', [AdminController::class, 'editUsers'])->name('users.edit');
+        Route::put('/update/{id}', [AdminController::class, 'updateUsers'])->name('users.update');
 
     });
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Add other resourceful controllers here
+
 });
 
+Route::prefix('admin')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.show');
+    Route::put('/profile/edit/{id}', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/change_pass/{id}', [ProfileController::class, 'changePass'])->name('profile.change_pass');
+    Route::delete('/profile/destroy/{id}', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/user/list', [AdminController::class, 'index'])->name('user.index');
+    Route::get('/role/list', [RoleController::class, 'index'])->name('role.index');
+
+    // ...
+
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 require __DIR__.'/auth.php';
+
